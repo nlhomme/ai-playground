01ce2789174031c845ea708af3167933a838ee4b
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2025-04-30 17:35:36 +0200

    Adding logger

--- chatbot-test.py
+++ chatbot-test.py
@@ -1,13 +1,35 @@
+import argparse
 import asyncio
 import os
-
+from logger import get_logger
 from mistralai import Mistral
 
 # VARIABLES
 userInput: str
 
+def parse_arguments():
+    """
+    Allow user to manually set the log level beetween DEBUG, INFO (default value), WARNING, ERROR AND CRITICAL
+
+    Example:
+    ```
+        chatbot-test.py --logLevel DEBUG
+    ```
+
+    Show a help message via the '-t' argument
+    """
+    parser = argparse.ArgumentParser(description="Requirements to use the AI playground chatbot")
+    parser.add_argument("--logLevel", required=False, type=str, default="INFO", help="Log level between DEBUG, INFO, WARNING, ERROR AND CRITICAL (default set to INFO)")
+    return parser.parse_args()
 
 async def main():
+    args = parse_arguments()
+
+    # Initiating logger
+    logLevel = args.logLevel
+    logger = get_logger("logs", "ai-playground", logLevel)
+    logger.info(f"Program has started")
+
     api_key = os.environ["MISTRAL_API_KEY"]
     model = "mistral-tiny"
 
@@ -26,10 +48,10 @@ async def main():
             print()
             userInput = input("Vous: ")
 
-
         # Quit program properly
         if "quit" in userInput:
             print("A la prochaine!")
+            logger.info(f"Program exit initiated by user")
             exit(0)
 
         # Send user input to Mistral and wait for its answer
@@ -42,6 +64,7 @@ async def main():
                   },
             ],
         )
+        logger.debug(f"User sended prompt :" + userInput)
 
         # Display Mistral answer
         print("Mistral: ", end = "")
